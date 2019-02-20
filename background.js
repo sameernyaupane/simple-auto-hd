6490f9a460754abf97a1137d3a14504d3a1d27d2
Sameer Nyaupane <sameernyaupane@outlook.com>
2019-02-20 14:13:05 +0545

    Add 50fps support, add links to the extension menu

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
 chrome.runtime.onInstalled.addListener(function(details) {
     if (details.reason == "install") {
-        chrome.management.getSelf(function(info) {
-            var ext_name = encodeURIComponent(info.name);
-            chrome.tabs.create({
-                url: 'https://sameernyaupane.com/youtube-auto-hd'
-            });
+        chrome.tabs.create({
+            url: 'https://opensourceaddons.com/addons/youtube-auto-hd/?installed'
+        });
+    } else if (details.reason == "update") {
+        var version = chrome.runtime.getManifest().version;
+        chrome.tabs.create({
+            url: 'https://opensourceaddons.com/addons/youtube-auto-hd/?v=' + version
         });
     }
 });
