28db52d2a57d2f92c5efe374783b4fa0848ecbe3
Sameer Nyaupane <sameernyaupane@outlook.com>
2023-06-11 15:15:40 +0545

    Fix issue with observer, add extension enable/disable feature

--- content.js
+++ content.js
@@ -1,288 +1,335 @@
-(function() {
+(function () {
+    // Wait for the page to trigger "yt-navigate-finish" event
+    document.addEventListener('yt-navigate-finish', function (event) {
+        // Trigger ovserver code only if the page contains '/watch' path
+        if (location.pathname === '/watch') {
+            runObserverIfExtensionEnabled();
+        }
+    });
+
+    function runObserverIfExtensionEnabled() {
+        chrome.storage.sync.get(['extensionEnabled'], function(result) {
+            if(result.extensionEnabled !== undefined && result.extensionEnabled === false) {
+                console.log('Simple Auto HD Extension Disabled. Please enable it through popup menu.');
+            } else {
+                console.log('Simple Auto HD Extension Enabled. Proceeding to initiate observer ');
+                initiateObserverAndObserve();
+            }
+        });
+    }
+
     // Configuration of the observer
     var config = {
-        childList    : true,
-        attributes   : true,
-        subtree      : true,
+        childList: true,
+        attributes: true,
+        subtree: true,
         characterData: true
     };
 
+    var theaterModeTitles = [
+        'Teatermodus',
+        'Teatr rejimi',
+        'Tampilan bioskop',
+        'Mod teater',
+        'Način rada za kino',
+        'Mode Cinema',
+        'Režim kina',
+        'Biograftilstand',
+        'Kinomodus',
+        'Kinorežiim',
+        'Cinema mode',
+        'Theater mode',
+        'Modo Cine',
+        'Modo cine',
+        'Antzoki modua',
+        'Mode cinéma',
+        'Kinematografski način rada',
+        'Imodi yethiyetha',
+        'Kvikmyndahúsastilling',
+        'Hali ya ukumbi wa filamu',
+        'Teātra režīms',
+        'Kino režimas',
+        'Mozi mód',
+        'Theatermodus',
+        'Modo cinema',
+        'Modo Teatro',
+        'Modul Cinema',
+        'Modaliteti i kinemasë',
+        'Način kina',
+        'Bioskopski režim',
+        'Teatteritila',
+        'Bioläge',
+        'Chế độ rạp chiếu phim',
+        'Sinema modu',
+        'Рэжым тэатра',
+        'Театр режими',
+        'Режим на кино сала',
+        'Широкий экран',
+        'Режим домашнього кінотеатру',
+        'Λειτουργία κινηματογράφου',
+        'Լայն էկրան',
+        'מצב קולנוע',
+        'تھیٹر وضع',
+        'وضع المسرح',
+        'حالت نمایش',
+        'थिएटर मोड',
+        'থিয়েটাৰ ম’ড',
+        'সিনেমা হল মোড',
+        'ਥੀਏਟਰ ਮੋਡ',
+        'થિયેટર મોડ',
+        'ଥିଏଟର୍‌ ମୋଡ୍‌',
+        'அரங்கு பயன்முறை',
+        'థియేటర్ మోడ్',
+        'ಥಿಯೇಟರ್ ಮೋಡ್',
+        'തീയേറ്റർ മോഡ്',
+        'රඟහල ප්‍රකාරය',
+        'โหมดโรงภาพยนตร์',
+        'ຮູບແບບໂຮງລະຄອນ',
+        'ရုပ်ရှင်ရုံ အနေအထား',
+        'თეატრალური რეჟიმი',
+        'ቲያትር ሁነታ',
+        'របៀប​រោងភាពយន្ត',
+        '剧场模式',
+        '劇院模式',
+        'シアター モード',
+        '영화관 모드(t)'
+    ];
+
+    var qualitiesArray = [
+        '4320p',
+        '4320p60',
+        '4320p50',
+        '4320p48',
+        '2160p',
+        '2160p60',
+        '2160p50',
+        '2160p48',
+        '1440p',
+        '1440p60',
+        '1440p50',
+        '1440p48',
+        '1080p',
+        '1080p60',
+        '1080p50',
+        '1080p48',
+        '720p',
+        '720p60',
+        '720p50',
+        '720p48',
+        '480p',
+        '360p',
+        '240p',
+        '144p',
+        'Auto'
+    ];
+
+    var qualityTitles = [
+        'Gehalte',
+        'Keyfiyyət',
+        'Kualitas',
+        'Kualiti',
+        'Kvalitet',
+        'Qualitat',
+        'Kvalita',
+        'Qualität',
+        'Kvaliteet',
+        'Quality',
+        'Calidad',
+        'Kalitatea',
+        'Kalidad',
+        'Qualité',
+        'Calidade',
+        'Kvaliteta',
+        'Ikhwalithi',
+        'Gæði',
+        'Ubora',
+        'Kvalitāte',
+        'Kokybė',
+        'Minőség',
+        'Kwaliteit',
+        'Sifati',
+        'Qualidade',
+        'Calitate',
+        'Cilësia',
+        'Kakovost',
+        'Laatu',
+        'Chất lượng',
+        'Kalite',
+        'Якасць',
+        'Сапаты',
+        'Квалитет',
+        'Качество',
+        'Якість',
+        'Ποιότητα',
+        'Որակ',
+        'איכות',
+        'معیار',
+        'الجودة',
+        'کیفیت',
+        'गुण',
+        'गुणवत्ता',
+        'क्वालिटी',
+        'গুণাগুণ',
+        'গুণমান',
+        'ਗੁਣਵੱਤਾ',
+        'ક્વૉલિટી',
+        'ଗୁଣବତ୍ତା',
+        'தரம்',
+        'క్వాలిటీ',
+        'ಗುಣಮಟ್ಟ',
+        'നിലവാരം',
+        'ගුණත්වය',
+        'คุณภาพ',
+        'ຄຸນນະພາບ',
+        'အရည်အသွေး',
+        'ხარისხი',
+        'ጥራት',
+        'គុណភាព​',
+        '画质',
+        '畫質',
+        '画質',
+        '화질'
+    ];
+
+    // Inititate observer
     function initiateObserverAndObserve() {
-        var observer = new MutationObserver(function(mutations) {
-                if (!document.contains(document.querySelector('.ytp-settings-button'))) {
-                    return;
-                }
+        var observer = new MutationObserver(function (mutations) {
+            if (!document.contains(document.querySelector('.ytp-settings-button'))) {
+                return;
+            }
 
-                observer.disconnect();
+            observer.disconnect();
 
-                // Run code
-                toggleTheaterMode();
+            // Run code after 100ms
+            setTimeout(() => {
                 selectPreferredQuality();
+                toggleTheaterMode();
+                listenToTheaterModeButton();
+            }, 100)
         });
 
         observer.observe(document.body, config);
     }
 
-    document.addEventListener('yt-navigate-finish', function(event) {
-        if(location.pathname === '/watch') {
-            initiateObserverAndObserve();
-        }
-    });
-
-    var toggleTheaterMode = function() {
-        chrome.storage.sync.get(['theaterMode'], function(result) { 
-            updateTheaterMode(result.theaterMode); 
-        });
-
-        var updateTheaterMode = function(theaterMode) {
-            var theaterModeTitles = [
-                'Teatermodus (t)',
-                'Teatr rejimi (t)',
-                'Tampilan bioskop (t)',
-                'Mod teater (t)',
-                'Način rada za kino (t)',
-                'Mode Cinema (t)',
-                'Režim kina (t)',
-                'Biograftilstand (t)',
-                'Kinomodus (t)',
-                'Kinorežiim (t)',
-                'Cinema mode (t)',
-                'Theater mode (t)',
-                'Modo Cine (t)',
-                'Modo cine (t)',
-                'Antzoki modua (t)',
-                'Mode cinéma (t)',
-                'Kinematografski način rada (t)',
-                'Imodi yethiyetha (t)',
-                'Kvikmyndahúsastilling (t)',
-                'Hali ya ukumbi wa filamu (t)',
-                'Teātra režīms (t)',
-                'Kino režimas (t)',
-                'Mozi mód (t)',
-                'Theatermodus (t)',
-                'Modo cinema (t)',
-                'Modo Teatro (t)',
-                'Modul Cinema (t)',
-                'Modaliteti i kinemasë (t)',
-                'Način kina (t)',
-                'Bioskopski režim (t)',
-                'Teatteritila (t)',
-                'Bioläge (t)',
-                'Chế độ rạp chiếu phim (t)',
-                'Sinema modu (t)',
-                'Рэжым тэатра (t)',
-                'Театр режими (t)',
-                'Режим на кино сала (t)',
-                'Широкий экран (t)',
-                'Режим домашнього кінотеатру (t)',
-                'Λειτουργία κινηματογράφου (t)',
-                'Լայն էկրան (t)',
-                'מצב קולנוע (t)',
-                'تھیٹر وضع (t)',
-                'وضع المسرح (t)',
-                'حالت نمایش (t)',
-                'थिएटर मोड (t)',
-                'থিয়েটাৰ ম’ড (t)',
-                'সিনেমা হল মোড (t)',
-                'ਥੀਏਟਰ ਮੋਡ (t)',
-                'થિયેટર મોડ (t)',
-                'ଥିଏଟର୍‌ ମୋଡ୍‌ (t)',
-                'அரங்கு பயன்முறை (t)',
-                'థియేటర్ మోడ్ (t)',
-                'ಥಿಯೇಟರ್ ಮೋಡ್ (t)',
-                'തീയേറ്റർ മോഡ് (t)',
-                'රඟහල ප්‍රකාරය (t)',
-                'โหมดโรงภาพยนตร์ (t)',
-                'ຮູບແບບໂຮງລະຄອນ (t)',
-                'ရုပ်ရှင်ရုံ အနေအထား (t)',
-                'თეატრალური რეჟიმი (t)',
-                'ቲያትር ሁነታ (t)',
-                'របៀប​រោងភាពយន្ត (t)',
-                '剧场模式 (t)',
-                '劇院模式 (t)',
-                'シアター モード（t）',
-                '영화관 모드(t)'
-            ];
-
-            if (theaterMode === undefined) {
-                chrome.storage.sync.set({theaterMode: false}, function() {});
-            }
-
+    // Listen to theater mode button for clicks
+    function listenToTheaterModeButton() {
+        var sizeButton = document.getElementsByClassName('ytp-size-button')[0];
+        
+        // Listen to theater mode changes
+        sizeButton.addEventListener('click', function(event) {
             var sizeButton = document.getElementsByClassName('ytp-size-button')[0];
-            var sizeButtonHasTheaterModeTitle = theaterModeTitles.includes(sizeButton.getAttribute('title'));
+            var title = sizeButton.getAttribute('data-title-no-tooltip');
 
-            if(theaterMode && sizeButtonHasTheaterModeTitle) {
-                sizeButton.click();
-            } else if(!theaterMode && !sizeButtonHasTheaterModeTitle) {
-                sizeButton.click();
-            }
-        }
-    };
+            var sizeButtonHasTheaterModeTitle = theaterModeTitles.includes(title);
 
-    var selectPreferredQuality = function() { 
-        var preferredQuality;
-        var qualitiesArray = [
-            '4320p', 
-            '4320p60', 
-            '4320p50', 
-            '4320p48', 
-            '2160p', 
-            '2160p60', 
-            '2160p50', 
-            '2160p48', 
-            '1440p',
-            '1440p60',
-            '1440p50',
-            '1440p48',
-            '1080p',
-            '1080p60',
-            '1080p50',
-            '1080p48',
-            '720p',
-            '720p60',
-            '720p50',
-            '720p48',
-            '480p',
-            '360p',
-            '240p',
-            '144p',
-            'Auto'
-        ];
-
-        var qualityTitles = [
-            'Gehalte',
-            'Keyfiyyət',
-            'Kualitas',
-            'Kualiti',
-            'Kvalitet',
-            'Qualitat',
-            'Kvalita',
-            'Qualität',
-            'Kvaliteet',
-            'Quality',
-            'Calidad',
-            'Kalitatea',
-            'Kalidad',
-            'Qualité',
-            'Calidade',
-            'Kvaliteta',
-            'Ikhwalithi',
-            'Gæði',
-            'Ubora',
-            'Kvalitāte',
-            'Kokybė',
-            'Minőség',
-            'Kwaliteit',
-            'Sifati',
-            'Qualidade',
-            'Calitate',
-            'Cilësia',
-            'Kakovost',
-            'Laatu',
-            'Chất lượng',
-            'Kalite',
-            'Якасць',
-            'Сапаты',
-            'Квалитет',
-            'Качество',
-            'Якість',
-            'Ποιότητα',
-            'Որակ',
-            'איכות',
-            'معیار',
-            'الجودة',
-            'کیفیت',
-            'गुण',
-            'गुणवत्ता',
-            'क्वालिटी',
-            'গুণাগুণ',
-            'গুণমান',
-            'ਗੁਣਵੱਤਾ',
-            'ક્વૉલિટી',
-            'ଗୁଣବତ୍ତା',
-            'தரம்',
-            'క్వాలిటీ',
-            'ಗುಣಮಟ್ಟ',
-            'നിലവാരം',
-            'ගුණත්වය',
-            'คุณภาพ',
-            'ຄຸນນະພາບ',
-            'အရည်အသွေး',
-            'ხარისხი',
-            'ጥራት',
-            'គុណភាព​',
-            '画质',
-            '畫質',
-            '画質',
-            '화질'
-        ];
-
-        chrome.storage.sync.get(['preferredQuality'], function(result) {
-            updateQuality(result.preferredQuality); 
-        });
-
-        var updateQuality = function(preferredQuality) {
-            if (preferredQuality === undefined) {
-                preferredQuality = 'best-available';
-                chrome.storage.sync.set({preferredQuality: preferredQuality}, function() {});
-            }
-
-            var settingsButton = document.getElementsByClassName('ytp-settings-button')[0];
-
-            settingsButton.click();
-
-            var buttons = document.getElementsByClassName('ytp-menuitem-label');
-
-            for (var i = 0; i < buttons.length; i++) {
-                if(qualityTitles.includes(buttons[i].innerHTML)) {
-                    buttons[i].click();
-                }
-            }
-
-            var targetItem;
-
-            if(preferredQuality === 'best-available') {
-                targetItem = document.querySelector('.ytp-quality-menu .ytp-menuitem-label');
+            if (sizeButtonHasTheaterModeTitle) {
+                setTheaterModeBoolean(true);
             } else {
-                var targetItems = document.querySelectorAll('.ytp-quality-menu .ytp-menuitem-label');
-
-                var loopCounter = 0;
-
-                function findTargetItem() {
-                    for (var i = 0; i < targetItems.length; i++) {
-                        var potentialTargetItem = targetItems[i].childNodes[0].childNodes[0];
-
-                        var quality = potentialTargetItem.innerHTML.split(' ')[0];
-
-                        if(quality === preferredQuality) {
-                            targetItem = potentialTargetItem;
-                        }
-                    }
-
-                    if(targetItem === undefined && loopCounter < 10) {
-                        var key = qualitiesArray.indexOf(preferredQuality);
-                        preferredQuality = qualitiesArray[key + 1];
-
-                        loopCounter++;
-
-                        return findTargetItem();
-                    }
-
-                    return targetItem;
-                }
-
-                targetItem = findTargetItem();
+                setTheaterModeBoolean(false);
             }
+        });
+    }
 
-            targetItem.click();
-        }
+    // Update storage value for theater mode
+    var setTheaterModeBoolean = function(theaterMode) {
+        chrome.storage.sync.set({theaterMode: theaterMode}, function() {});
+    }
+
+    // Get storage value and update theater mode
+    var toggleTheaterMode = function () {
+        chrome.storage.sync.get(['theaterMode'], function (result) {
+            updateTheaterMode(result.theaterMode);
+        });
     };
 
-    chrome.storage.onChanged.addListener(function(changes, namespace) {
+    // Update theater mode
+    var updateTheaterMode = function (theaterMode) {
+        if (theaterMode === undefined) {
+            chrome.storage.sync.set({ theaterMode: false }, function () { });
+        }
+
+        var sizeButton = document.getElementsByClassName('ytp-size-button')[0];
+        var sizeButtonHasTheaterModeTitle = theaterModeTitles.includes(sizeButton.getAttribute('data-title-no-tooltip'));
+
+        if (theaterMode && sizeButtonHasTheaterModeTitle) {
+            sizeButton.click();
+        } else if (!theaterMode && !sizeButtonHasTheaterModeTitle) {
+            sizeButton.click();
+        }
+    }
+
+    // Get storage value and update to given quality
+    var selectPreferredQuality = function () {
+        var preferredQuality;
+
+        chrome.storage.sync.get(['preferredQuality'], function (result) {
+            updateQuality(result.preferredQuality);
+        });
+    };
+
+    // Update to given quality
+    var updateQuality = function (preferredQuality) {
+        if (preferredQuality === undefined) {
+            preferredQuality = 'best-available';
+            chrome.storage.sync.set({ preferredQuality: preferredQuality }, function () { });
+        }
+
+        var settingsButton = document.getElementsByClassName('ytp-settings-button')[0];
+
+        settingsButton.click();
+
+        var buttons = document.getElementsByClassName('ytp-menuitem-label');
+
+        for (var i = 0; i < buttons.length; i++) {
+            if (qualityTitles.includes(buttons[i].innerHTML)) {
+                buttons[i].click();
+            }
+        }
+
+        var targetItem;
+
+        if (preferredQuality === 'best-available') {
+            targetItem = document.querySelector('.ytp-quality-menu .ytp-menuitem-label');
+        } else {
+            var targetItems = document.querySelectorAll('.ytp-quality-menu .ytp-menuitem-label');
+
+            targetItem = findTargetItem(preferredQuality, targetItems);
+        }
+
+        targetItem.click();
+    }
+
+    // Find the target quality
+    function findTargetItem(preferredQuality, targetItems) {
+        var targetItem = '';
+        
+        for (var i = 0; i < targetItems.length; i++) {
+            var potentialTargetItem = targetItems[i].childNodes[0].childNodes[0];
+
+            var quality = potentialTargetItem.innerHTML.split(' ')[0];
+
+            if (quality === preferredQuality) {
+                targetItem = potentialTargetItem;
+            }
+        }
+
+        var key = qualitiesArray.indexOf(preferredQuality);
+
+        if (targetItem === '' && (qualitiesArray[key + 1] !== undefined)) {
+            preferredQuality = qualitiesArray[key + 1];
+
+            return findTargetItem(preferredQuality, targetItems);
+        }
+
+        return targetItem;
+    }
+
+    // Listen for chrome storage changes
+    chrome.storage.onChanged.addListener(function (changes, namespace) {
         for (key in changes) {
-            if(key === 'theaterMode') {
+            if (key === 'theaterMode') {
                 toggleTheaterMode();
-            } else if(key === 'preferredQuality') {
+            } else if (key === 'preferredQuality') {
                 selectPreferredQuality();
             }
         }
--- popup.js
+++ popup.js
@@ -1,10 +1,35 @@
 (function() {
-    var version = chrome.runtime.getManifest().version; 
-    var theaterModeElement = document.getElementById('theater-mode');
+    var version                 = chrome.runtime.getManifest().version;
+    var theaterModeElement      = document.getElementById('theater-mode');
+    var extensionStatusElement  = document.getElementById('extension-status');
+    var extensionEnabledElement = document.getElementById('extension-enabled');
     var preferredQualityElement = document.getElementById('preferred-quality');
+    var wrapperElement          = document.getElementsByClassName('wrapper')[0];
 
     var headerElem = document.querySelector('header .version');
     headerElem.innerText = 'v' + version;
+
+    function disableElements() {
+        wrapperElement.classList.add('wrapper-enabled');
+        document.body.style.backgroundColor = 'grey';
+        extensionStatusElement.innerText = 'Extension Disabled';
+    }
+
+    function enableElements() {
+        wrapperElement.classList.remove('wrapper-enabled');
+        document.body.style.backgroundColor = '#262a32';
+        extensionStatusElement.innerText = 'Extension Enabled';
+    }
+
+    chrome.storage.sync.get(['extensionEnabled'], function(result) { 
+        if(result.extensionEnabled !== undefined && result.extensionEnabled === false) {
+            extensionEnabledElement.checked = false; 
+            disableElements();
+        } else {
+            enableElements();
+            extensionEnabledElement.checked = true;
+        }
+    });
 
     chrome.storage.sync.get(['preferredQuality'], function(result) { 
         if(result.preferredQuality !== undefined) {
@@ -32,7 +57,21 @@
         }
     });
 
+    extensionEnabledElement.addEventListener('change', function(event) {
+        if (event.target.checked) {
+            setExtensionEnabledBoolean(true);
+            enableElements();
+        } else {
+            setExtensionEnabledBoolean(false);
+            disableElements();
+        }
+    });
+
     var setTheaterModeBoolean = function(theaterMode) {
         chrome.storage.sync.set({theaterMode: theaterMode}, function() {});
+    }
+
+    var setExtensionEnabledBoolean = function(extensionEnabled) {
+        chrome.storage.sync.set({extensionEnabled: extensionEnabled}, function() {});
     }
 })();
